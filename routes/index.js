b80574edf3d405cd34ee43c7146733eab937ebe6
ste001 <asso99@hotmail.com>
2019-05-04 14:19:11 +0200

    Add Index with messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hello, World!",
+    user: "Nathan",
+    added: new Date()
+  },
+  {
+    text: "Second text",
+    user: "Bobby",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index', messages: messages });
 });
 
 module.exports = router;
